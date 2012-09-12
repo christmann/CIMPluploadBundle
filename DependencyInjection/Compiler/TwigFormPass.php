c90d94a2708bc6bb90e8b91a6a18ddb6b546792b
Patrick Bußmann <patrick.bussmann@christmann.info>
2012-09-12 08:47:10 +0200

    Changed BaseBundle to Localdev Bundle

--- DependencyInjection/Compiler/TwigFormPass.php
+++ DependencyInjection/Compiler/TwigFormPass.php
@@ -10,16 +10,20 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
  */
 class TwigFormPass implements CompilerPassInterface
 {
+
+	/**
+	 * {@inheritDoc}
+	 */
 	public function process(ContainerBuilder $container)
 	{
 		if ($container->hasParameter('twig.form.resources'))
 		{
 			$container->setParameter('twig.form.resources', array_merge(
-				$container->getParameter('twig.form.resources'),
-				array(
-					 'CIMPluploadBundle:Form:form.html.twig'
-				)
-			));
+							$container->getParameter('twig.form.resources'), array(
+								'CIMPluploadBundle:Form:form.html.twig'
+							)
+					));
 		}
 	}
+
 }
