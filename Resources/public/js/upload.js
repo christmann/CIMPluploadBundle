e51a0905772695f5fb504d629bcf5d215c7f2c54
Patrick Bußmann <patrick.bussmann@christmann.info>
2013-01-22 15:16:07 +0100

    Updated plupload for jquery 1.9.*

--- Resources/public/js/upload.js
+++ Resources/public/js/upload.js
@@ -131,7 +131,7 @@
 		uploader.bind('Error', $.UserUpload.Error);
 
 		uploader.bind('FileUploaded', $.UserUpload.FileUploaded);
-		$('#' + options.container).find('.upload_remove').live('click', $.UserUpload.FileRemove);
+		$('#' + options.container).find('.upload_remove').on('click', $.UserUpload.FileRemove);
 	};
 
 	$.fn.UserUpload.defaults = {
